18c27ad9537b4ee9f39e192eb3a8702ad5e9d15b
XiaoDev <jingyuanx@tujia.com>
2019-10-31 14:37:01 +0800

    init

---
+++ XDPhoto/Classes/XDPhotoPicker/XDPhotoPicker.h
@@ -0,0 +1,16 @@
+//
+//  XDPhotoPicker.h
+//  Pods-XDPhoto_Example
+//
+//  Created by XiaoDev on 2019/10/31.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XDPhotoPicker : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XDPhoto/Classes/XDPhotoPicker/XDPhotoPicker.m
@@ -0,0 +1,12 @@
+//
+//  XDPhotoPicker.m
+//  Pods-XDPhoto_Example
+//
+//  Created by XiaoDev on 2019/10/31.
+//
+
+#import "XDPhotoPicker.h"
+
+@implementation XDPhotoPicker
+
+@end
